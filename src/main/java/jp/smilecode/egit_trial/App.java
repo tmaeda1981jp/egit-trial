a64d513cec56ade00f8f1cc4f16df224281414fb
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-05-07 03:31:40 +0900

    first commit

---
+++ src/main/java/jp/smilecode/egit_trial/App.java
@@ -0,0 +1,13 @@
+package jp.smilecode.egit_trial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
